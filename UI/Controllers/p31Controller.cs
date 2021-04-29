edbad0fe35bd47f7bb7c91f8b9c59dd6aa6e5638
Jiri Theimer <jiri.theimer@gmail.com>
2021-04-29 23:21:27 +0200

    a

---
+++ UI/Controllers/p31Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class p31Controller : BaseController
+    {
+        public IActionResult Info(int pid)
+        {
+            var v = new p31Info() { pid = pid };
+            v.Rec = Factory.p31WorksheetBL.Load(v.pid);
+
+            return View(v);
+        }
+    }
+}
---
+++ UI/Models/Record/p31Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Models
+{
+    public class p31Info:BaseViewModel
+    {
+        public BO.p31Worksheet Rec { get; set; }
+        public int pid { get; set; }
+    }
+}
