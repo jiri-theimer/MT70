5931155456b53a4773d84139e84f0b12bedd51c0
Jiri Theimer <jiri.theimer@gmail.com>
2021-05-07 21:56:15 +0200

    a

--- BO/model/query/myQueryJ11.cs
+++ BO/model/query/myQueryJ11.cs
@@ -7,6 +7,7 @@ namespace BO
     public class myQueryJ11:baseQuery
     {
         public int j02id { get; set; }
+        public bool? notall { get; set; }
 
         public myQueryJ11()
         {
@@ -19,7 +20,10 @@ namespace BO
             {
                 AQ("a.j11ID IN (select j11ID FROM j12Team_Person where j02ID=@j02id)", "j02id", this.j02id);
             }
-
+            if (this.notall == true)
+            {
+                AQ("a.j11IsAllPersons=0", null, null);
+            }
 
             return this.InhaleRows();
 
--- UI/Models/p31view/daylineViewModel.cs
+++ UI/Models/p31view/daylineViewModel.cs
@@ -11,11 +11,15 @@ namespace UI.Models.p31view
         public DateTime d1 { get; set; }
         public DateTime d2 { get; set; }
         public IEnumerable<BO.j02Person> lisJ02 { get; set; }
+        
         public IEnumerable<BO.p31Worksheet> lisP31 { get; set; }
         public IEnumerable<BO.p31WorksheetTimelineDay> lisSums { get; set; }
         public string j07IDs { get; set; }
+        public string SelectedPositions { get; set; }
         public string j02IDs { get; set; }
+        public string SelectedPersons { get; set; }
         public string j11IDs { get; set; }
+        public string SelectedTeams { get; set; }
 
     }
 }
