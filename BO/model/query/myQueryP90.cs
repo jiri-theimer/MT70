4948f43883d139f6abe42b016f298bb767f488ce
Jiri Theimer <jiri.theimer@gmail.com>
2021-05-25 16:38:40 +0200

    a

--- BO/model/query/myQueryP90.cs
+++ BO/model/query/myQueryP90.cs
@@ -39,8 +39,39 @@ namespace BO
 
             }
 
+            Handle_MyDisponible();
+
             return this.InhaleRows();
 
+        }
+
+        private void Handle_MyDisponible()
+        {
+            if ((this.CurrentUser.IsAdmin || this.CurrentUser.TestPermission(x53PermValEnum.GR_P90_Owner) || this.CurrentUser.TestPermission(x53PermValEnum.GR_P90_Reader)))
+            {
+                return; //přístup ke všem zálohám v systému
+            }
+
+            string s = "EXISTS (SELECT 1 FROM x73EntityRole_DelegationCache xa INNER JOIN x69EntityRole_Assign xb ON xa.x67ID_Master=xb.x67ID AND xa.x73MasterPid=xb.x69RecordPID";
+            s += " WHERE xa.x73SlavePrefix='p90'";
+            s += " AND xa.x73SlavePid=a.p90ID";
+
+            if (string.IsNullOrEmpty(this.CurrentUser.j11IDs))
+            {
+                s += " AND (xb.j02ID=@j02id_query";
+            }
+            else
+            {
+                s += $" AND (xb.j02ID=@j02id_query OR xb.j11ID IN ({this.CurrentUser.j11IDs})"; //přihlášený uživatel je také členem týmů
+            }
+            if (this.CurrentUser.IsMasterPerson)    //přihlášený uživatel má pod sebou podřízené uživatele
+            {
+                s += " OR xb.j02ID IN (select j02ID_Slave FROM j05MasterSlave WHERE j02ID_Slave IS NOT NULL AND j02ID_Master=@j02id_query)";
+            }
+            s += ")";
+            s += ")";
+
+            AQ(s, "j02id_query", this.CurrentUser.j02ID);
         }
     }
 }
